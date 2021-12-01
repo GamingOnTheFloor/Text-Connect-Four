b8a40921fcb0404260c2b5b5d1c56ce7eb452a3a
GamingOnTheFloor <GamingOnTheFloor.GOTF@gmail.com>
2021-12-01 14:54:32 -0700

    Added Capability for more than 2 players (WIP)

--- text_connect_four.py
+++ text_connect_four.py
@@ -5,6 +5,20 @@ board = {1: "|1|2|3|4|5|\n",
         4: "|#|#|#|#|#|\n",
         5: "|#|#|#|#|#|\n",
         6: "|#|#|#|#|#|\n"}
+player = ""
+amount = int(input("How many player's do you want to play with?"))
+def player_setter(player, amount):
+    i = 0
+    while i < amount:
+        temp_player = input(f"What character do you want player {len(player)+1} to be?")
+        while len(temp_player) > 1:
+            print("Sorry, you can only set one character to your player.")
+            temp_player = input(f"What character do you want player {len(player)+1} to be?")
+        player += temp_player
+        print(player)
+        i+=1
+    return player
+
 def print_board(board):
     i = 1
     printed_board = '\n'
@@ -24,7 +38,7 @@ def lowest_unoccupied_line(column, board):
             lowest_line = line
             return lowest_line
 
-def replace(line, column, board):
+def replace(line, column, board, player_number, player):
     try:
         str_line = board[line]
     except Exception as e:
@@ -34,14 +48,16 @@ def replace(line, column, board):
     new_str_line = ''
     while i < len(str_line):
         if i == column*2-1:
-            new_str_line += "X"
+            new_str_line += player[player_number]
         else:
             new_str_line += str_line[i]
         i+=1
     board[line] = new_str_line
     return board[line]
 
-def main():
+def main(player_number):
+    if player_number >= len(player):
+        player_number = 0
     print(print_board(board))
     user = 0
     while user not in (1,2,3,4,5):
@@ -52,6 +68,9 @@ def main():
                 print(f"Error: '{user}' not in range 1-5\n")
         except Exception as e:
             print(f"Error: '{user}' not in range 1-5\n")
-    replace(lowest_unoccupied_line(user, board), user, board)
-    main()
-main()
+    replace(lowest_unoccupied_line(user, board), user, board, player_number, player)
+    player_number += 1
+    main(player_number)
+player_number = 0
+player = player_setter(player, amount)
+main(player_number)
