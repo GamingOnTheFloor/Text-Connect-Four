5357a7d92d6b32be3f60db0a912749e3451963f7
24gmyers <duhbobby11@gmail.com>
2022-01-05 14:56:10 -0700

    Start_screen issues in main

--- text_connect_four.py
+++ text_connect_four.py
@@ -16,30 +16,29 @@ player_number = 0
 
 amount = ""
 
-def start_screen(amount):
-    amount = input("How many player's do you want to play with?\n")
-    player_setter(player, amount)
+def start_screen(amount, player):
+    try:
+        amount = int(input("How many player's do you want to play with?\n"))
+    except Exception as f:
+        print (f"The amount of players '{amount}' must be a number")
+        start_screen(amount)
+    player = player_setter(player, amount)
+    return player
 
 def player_setter(player, amount):
-    if type(amount) == str:
-        try:
-            amount = int(amount)
-            i = 0
-            while i < amount:
+    i = 0
+    while i < int(amount):
+        temp_player = input(f"What character do you want player {len(player)+1} to be?\n")
+        while len(temp_player) > 1 or temp_player in player:
+            if len(temp_player) > 1:
+                print("Sorry, you can only set one character to your player.")
                 temp_player = input(f"What character do you want player {len(player)+1} to be?\n")
-                while len(temp_player) > 1:
-                    print("Sorry, you can only set one character to your player.")
-                    temp_player = input(f"What character do you want player {len(player)+1} to be?\n")
-                while temp_player in player:
-                    print(f"Sorry, {temp_player} is already a used character by another player.")
-                    temp_player = input(f"What character do you want player {len(player)+1} to be\n?")
-                player += temp_player
-                print(player)
-                i+=1
-            return player
-        except Exception as f:
-            print (f"the amount of players ({amount}) must be a number")
-            start_screen(amount)
+            if temp_player in player:
+                print(f"Sorry, '{temp_player}' is already a used character by another player.")
+                temp_player = input(f"What character do you want player {len(player)+1} to be?\n")
+        player += temp_player
+        i+=1
+    return player
 
 def print_board(board):
     i = 1
@@ -73,6 +72,7 @@ def replace(line, column, board, player_number, player):
     new_str_line = ''
     while i < len(str_line):
         if i == column*2-1:
+            print(f"{player} LEFT OF HERE")
             new_str_line += player[player_number]
         else:
             new_str_line += str_line[i]
@@ -81,23 +81,24 @@ def replace(line, column, board, player_number, player):
     return board[line]
 
 def main(player_number, player):
-    start_screen(amount)
-    player = player_setter(player, amount)
+    player = start_screen(amount, player)
     if player_number >= len(player):
         player_number = 0
     print(print_board(board))
     user = 0
-    while user not in (1,2,3,4,5,6,7):
+    while user not in range(1,len(board)):
+#   while user not in (1,2,3,4,5,6,7):
         user = input('What column would you like to drop your piece in?\n')
         try:
             user = int(user)
-            if user not in (1,2,3,4,5,6,7):
+            if user not in range(1,len(board)):
+#           if user not in (1,2,3,4,5,6,7):
                 if user == "q":
                     print ("goodbye")
                 else:
-                    print(f"Error: '{user}' not in range 1-7\n")
+                    print(f"Error: '{user}' not in range 1-{len(board)}\n")
         except Exception as e:
-            print(f"Error: '{user}' not in range 1-7\n")
+            print(f"Error: '{user}' not in range 1-{len(board)}\n")
     check = replace(lowest_unoccupied_line(user, board), user, board, player_number, player)
     if check != False:
         player_number += 1
